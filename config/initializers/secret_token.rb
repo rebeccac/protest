1184f007c239010164e8f5731bed8ec039f6b064
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-08-13 20:20:24 +0930

    Initialise repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Protestaustralia::Application.config.secret_key_base = 'ad33a4f344b21615d8cd99e0c56cf20f96331121bd7789e1082a13ec9a0f99b2d608927b036e687458e0421d35263efac89f9a8aecfa3dec4c49468bf411ec25'
