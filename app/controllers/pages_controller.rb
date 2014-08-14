f4a1712c61cf59dbca8d6c2db57a646e5feb5a9d
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-08-14 23:09:03 +0930

    Related sites page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def related_sites
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Protestaustralia::Application.routes.draw do
   devise_for :users
   root to: 'pages#index'
 
+  get "pages/related_sites"
+
   resources :protests
 
   match '/protests', to: 'protests#index', via: [:get]
   match '/new', to: 'protests#new', via: [:get]
+  match '/related_sites', to: 'pages#related_sites', via: [:get]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
