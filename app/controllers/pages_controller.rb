c2ae113cbea048c498bc80aaa0e67170ca646c0a
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-09-29 16:53:42 +0930

    Front page recent protests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-     @protests = Protest.all(:order => "id desc", :limit => 5).reverse
+     @recent_protests = Protest.all(:order => "id desc", :limit => 5).reverse
      render :layout => 'frontpage'
   end
 
