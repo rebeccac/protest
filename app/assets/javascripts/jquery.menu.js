6022ceb75386fcde69935b2f9b175e7848fa90ab
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-09-30 20:16:45 +0930

    Dropdown menu

--- app/assets/javascripts/jquery.menu.js
+++ app/assets/javascripts/jquery.menu.js
@@ -1,8 +1,6 @@
-$( document ).ready(function() {
-   $("#menu-button").click( function(e) {
+$(document).ready(function () {
+   $('#menu-icon').click(function(e) {
       e.preventDefault();
-      $("#slider").toggleClass("slidermenu-left").toggleClass("slidermenu-pushed");
-      $("#menu-button").toggleClass("menu-button-left").toggleClass("menu-button-pushed");
-      $(".page-body").toggleClass("page-body-left").toggleClass("page-body-pushed");
+      $('#dropdown-menu').slideToggle('medium');
    });
 });
