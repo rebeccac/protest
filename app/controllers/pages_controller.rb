d5026261c8fc39b21ae5be0dc058186cec4bfad0
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-10-03 11:57:33 +0930

    Merged front page & application layouts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def index
      @recent_protests = Protest.all(:order => "id desc", :limit => 6).reverse
-     render :layout => 'frontpage'
+   #   render :layout => 'frontpage'
   end
 
   def related_sites
